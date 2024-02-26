55b16a9e28c866968ae45190a95308e5b87dca10
Mihalycsik-Laszlo <mihalycsik@gmail.com>
2024-02-26 22:17:47 +0100

    login-form

--- 01-crypto-change/src/app/components/start/start.component.ts
+++ 01-crypto-change/src/app/components/start/start.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
-  styleUrls: ['./start.component.css']
+  styleUrls: ['./start.component.css'],
 })
 export class StartComponent {
+  loginForm: FormGroup = new FormGroup({
+    userName: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
+  login() {}
 }
